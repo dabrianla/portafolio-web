5e302b225062b9c2a31b9e8a4ee67c727e1dff8a
dabrianla <81344554+dabrianla@users.noreply.github.com>
2025-08-09 03:24:40 -0400

    bd

--- portafolio/settings.py
+++ portafolio/settings.py
@@ -77,7 +77,11 @@ WSGI_APPLICATION = 'portafolio.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': dj_database_url.config(
+        default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}",
+        conn_max_age=600,
+        ssl_require=False  # Pon True si Render lo pide para PostgreSQL
+    )
 }
 
 
